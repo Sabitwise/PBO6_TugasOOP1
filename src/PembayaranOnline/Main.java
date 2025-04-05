ad2236ebba0ce10d7d3b4c6aa90528424388d653
Ary Boby <ary.boby@sinarmasmining.com>
2025-04-05 12:32:40 +0700

    test

--- src/PembayaranOnline/Main.java
+++ src/PembayaranOnline/Main.java
@@ -2,20 +2,19 @@ package PembayaranOnline;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //siti
-
+        //siti's transaction
         Payment Siti = new Payment(3500000, "Credit Card");
         Siti.processPayment("Credit Card");
         Siti.headline();
         Siti.display();
         Siti.boundaries();
-        //ciciripi
+        //ciciripi's transaction
         Payment Ciciripi = new Payment (10000000, "Voucher");
         Ciciripi.processPayment("Voucher", 0.15);
         Ciciripi.headline();
         Ciciripi.display();
         Ciciripi.boundaries();
-        //asnawi
+        //asnawi's transaction
         Payment Asnawi = new Payment (1200000, "Transfer Bank");
         Asnawi.processPayment("Transfer Bank");
         Asnawi.headline();
